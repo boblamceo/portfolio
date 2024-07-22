0ae15c2a69cac9a31f1e383c200e88470102669c
boblamceo <boblam.ceo@gmail.com>
2024-07-22 22:18:59 +0800

    tailwind and other styles

--- app/page.js
+++ app/page.js
@@ -3,40 +3,31 @@ import {
     MouseParallaxChild,
     MouseParallaxContainer,
 } from "react-parallax-mouse";
-import "./styles.css";
 
 export default function App() {
     return (
         <>
-            <div className="h-full w-full bg-slate-800 text-white overflow-hidden">
+            <div className="h-full w-full bg-black text-white overflow-hidden">
                 <div className="h-full flex flex-col justify-center align-middle">
                     <MouseParallaxContainer
-                        className="parallax"
                         containerStyle={{
                             width: "100%",
                             display: "grid",
                             gridTemplateColumns: "auto auto auto auto auto",
                         }}
+                        className="w-screen h-screen"
                         globalFactorX={0.3}
                         globalFactorY={0.3}
-                        resetOnLeave
                     >
                         <MouseParallaxChild
                             factorX={0.6}
                             factorY={0.1}
-                            className="bg-center scale-125 absolute bg-auto w-full h-full"
+                            className="bg-[url('/earth.png')] bg-center absolute bg-auto w-[150vw] h-[150vh]"
                         />
                         <MouseParallaxChild
                             factorX={0.5}
                             factorY={0.5}
-                            style={{
-                                filter: "invert(1)",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                width: "auto",
-                                height: "100%",
-                            }}
+                            className="flex align-middle justify-center"
                         >
                             <h1>Bob Lam</h1>
                             <div>Start</div>

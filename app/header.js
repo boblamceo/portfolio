65db15492f714c7df838166e18b3a8843c701824
boblamceo <boblam.ceo@gmail.com>
2024-07-23 20:41:37 +0800

    finish header

--- app/header.js
+++ app/header.js
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import { Montserrat, Open_Sans } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     subsets: ["latin"],
@@ -12,15 +15,22 @@ const opensans = Open_Sans({
 });
 
 const Header = () => {
+    const pathname = usePathname();
     return (
         <div className={`flex flex-row p-[1.5vw] ${montserrat.className} `}>
             <h1 className={"text-[2vw] text-white"}>Bob Lam</h1>{" "}
-            <div className="ml-[20vw] text-white">
-                <div className={"text-[1vw]"}>About</div>
-                <div className={"text-[1vw]"}>Projects</div>
-                <div className={"text-[1vw]"}>Awards</div>
-                <div className={"text-[1vw]"}>Contact</div>
-            </div>
+            <ul className="ml-[20vw] text-white">
+                <li
+                    className={`text-[1vw] header-bt ${
+                        pathname === "/about" && "font-bold"
+                    }`}
+                >
+                    About
+                </li>
+                <li className={`text-[1vw] header-bt`}>Projects</li>
+                <li className={`text-[1vw] header-bt`}>Awards</li>
+                <li className={`text-[1vw] header-bt`}>Contact</li>
+            </ul>
         </div>
     );
 };

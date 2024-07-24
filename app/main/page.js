22095a32e207ecf5b58dc375f1bb78b2ad26bbc5
boblamceo <boblam.ceo@gmail.com>
2024-07-24 18:55:58 +0800

    figured out how to detect scroll

--- app/main/page.js
+++ app/main/page.js
@@ -4,6 +4,7 @@ import React, { useState, useRef, useEffect } from "react";
 import Header from "../header";
 import About from "../aboutPage";
 import Projects from "../projectPage";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Main = () => {
     const aboutRef = useRef(null);
@@ -11,53 +12,26 @@ const Main = () => {
     const [isVisible, setIsVisible] = useState("/about");
 
     useEffect(() => {
-        const handleScroll = () => {
-            if (aboutRef.current) {
-                const rect = aboutRef.current.getBoundingClientRect();
-                const isVisible =
-                    rect.top >= 0 &&
-                    rect.left >= 0 &&
-                    rect.bottom <=
-                        (window.innerHeight ||
-                            document.documentElement.clientHeight) &&
-                    rect.right <=
-                        (window.innerWidth ||
-                            document.documentElement.clientWidth);
-                if (isVisible) {
-                    setIsVisible("/about");
-                    console.log("hi");
-                }
-            }
-            if (projectsRef.current) {
-                const rect = projectsRef.current.getBoundingClientRect();
-                const isVisible =
-                    rect.top >= 0 &&
-                    rect.left >= 0 &&
-                    rect.bottom <=
-                        (window.innerHeight ||
-                            document.documentElement.clientHeight) &&
-                    rect.right <=
-                        (window.innerWidth ||
-                            document.documentElement.clientWidth);
-                if (isVisible) {
-                    setIsVisible("/projects");
-                    console.log("bye");
-                }
-            }
-        };
-
         window.addEventListener("scroll", handleScroll);
-        // Initial check on component mount
-        handleScroll();
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+        return () => window.removeEventListener("scroll", handleScroll);
     }, []);
+
+    const handleScroll = () => {
+        console.log(
+            window.scrollY,
+            window.innerHeight + window.innerWidth * 0.13
+        );
+        if (window.scrollY < window.innerHeight + window.innerWidth * 0.13) {
+            setIsVisible("/about");
+        } else {
+            setIsVisible("/projects");
+        }
+    };
     return (
         <div className="bg-slate-800 w-screen flex flex-col">
             <Header page={isVisible} /> <About innerref={aboutRef} />{" "}
             <Projects innerref={projectsRef} />
+            <div>hi</div>
         </div>
     );
 };

e99f0c97a074f4b7fa376e25d9b4bb4d5aa551b5
boblamceo <boblam.ceo@gmail.com>
2024-07-26 21:42:55 +0800

    animations

--- app/main/page.js
+++ app/main/page.js
@@ -5,12 +5,14 @@ import Header from "../header";
 import About from "../aboutPage";
 import Projects from "../projectPage";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import loadingsrc from "../../public/loading.gif";
 
 const Main = () => {
     const aboutRef = useRef(null);
     const projectsRef = useRef(null);
     const [isVisible, setIsVisible] = useState("/about");
-
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
@@ -27,6 +29,16 @@ const Main = () => {
             setIsVisible("/projects");
         }
     };
+    if (loading) {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+        return (
+            <div className="w-screen h-screen flex justify-center items-center  bg-slate-800">
+                <Image src={loadingsrc} className="w-[10vw]" />
+            </div>
+        );
+    }
     return (
         <motion.div
             className="bg-slate-800 w-screen flex flex-col"

97bbb7deabca1140a89162a59301c14408189f81
boblamceo <boblam.ceo@gmail.com>
2024-07-26 14:18:09 +0800

    it is somehow working now

--- app/api/limewire.js
+++ app/api/limewire.js
@@ -1,27 +1,37 @@
 require("dotenv").config();
 
-import { createApi } from "unsplash-js";
 import axios from "axios";
 
-const clientId = process.env.API_KEY;
-
-const LIMEWIRE_ROOT = "https://api.limewire.com/api/image/generation";
+const apiKey = process.env.API_KEY;
 
 export const getPhotosByQuery = async ({ query }) => {
-    const { data } = await axios.post(
-        LIMEWIRE_ROOT,
-        {
-            prompt: query,
-            aspect_ratio: "3:2",
+    const options = {
+        method: "POST",
+        url: "https://api.segmind.com/v1/sdxl1.0-txt2img",
+        headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json",
         },
-        {
-            headers: {
-                "Content-Type": "application/json",
-                "X-Api-Version": "v1",
-                Accept: "application/json",
-                Authorization: `Bearer ${clientId}`,
-            },
-        }
-    );
-    return data;
+        responseType: "arraybuffer",
+        data: {
+            prompt: query,
+            seed: 17123564234,
+            num_inference_steps: 20,
+            negative_prompt: "NONE",
+            samples: "1",
+            guidance_scale: "7.5",
+            strength: "1",
+            shape: 512,
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        // convert raw blob as ArrayBuffer to an image blob with MIME type
+        const imageBlob = new Blob([response.data], { type: "image/jpeg" });
+        // console.log(response, imageBlob);
+        return imageBlob;
+    } catch (error) {
+        console.error("Error while fecthing Gen AI model API", error);
+    }
 };

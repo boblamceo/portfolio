89f904d19c024c3a8e7ce473a4c9a6da52b400d8
boblamceo <boblam.ceo@gmail.com>
2024-08-02 16:10:04 +0800

    set up routing system

--- app/description/page.js
+++ app/description/page.js
@@ -1,11 +1,12 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 
-const Description = (props) => {
-    useEffect(() => {
-        console.log(props.router.query.name);
-    }, [props.router.query]);
-    return <h1>HELLO</h1>;
+const Description = () => {
+    const searchParams = useSearchParams();
+
+    const name = searchParams.get("name");
+    return <h1>{name}</h1>;
 };
 
 export default Description;
--- app/slide.js
+++ app/slide.js
@@ -2,7 +2,7 @@
 import { ArrowForward } from "@mui/icons-material";
 import { motion } from "framer-motion";
 import { Montserrat, Open_Sans } from "next/font/google";
-import Router from "next/router";
+import Link from "next/link";
 import React, { useState } from "react";
 
 const montserrat = Montserrat({
@@ -24,90 +24,85 @@ const Slide = ({ date, name, type, images, videos }) => {
             ? "bg-red-500"
             : "bg-green-500";
     return (
-        <motion.div
-            className="flex flex-row mt-[10vw] ml-[5vw] text-white relative z-0 hover:cursor-pointer"
-            viewport={{ once: true }}
-            initial={{ x: "-100%", opacity: 0, scale: 1 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            whileHover={{ scale: 1.02 }}
-            transition={{
-                duration: 2,
-                scale: { duration: 0.5, type: "spring" },
-            }}
-            onMouseEnter={() => {
-                setHover(true);
-            }}
-            onMouseLeave={() => {
-                setHover(false);
-            }}
-            onClick={() => {
-                Router.push({
-                    pathname: "/description",
-                    query: {
-                        date,
-                        name,
-                        type,
-                        images,
-                        videos,
-                    },
-                });
+        <Link
+            href={{
+                pathname: "/description",
+                query: { date, name, type, images, videos },
             }}
         >
-            <div
-                className={`${color} w-[50vw] rounded-lg p-[1.5vw] h-[30vw] shadow-[0px_2vh_3vh_0.5vh_#fff]`}
+            <motion.div
+                className="flex flex-row mt-[10vw] ml-[5vw] text-white relative z-0 hover:cursor-pointer"
+                viewport={{ once: true }}
+                initial={{ x: "-100%", opacity: 0, scale: 1 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                whileHover={{ scale: 1.02 }}
+                transition={{
+                    duration: 2,
+                    scale: { duration: 0.5, type: "spring" },
+                }}
+                onMouseEnter={() => {
+                    setHover(true);
+                }}
+                onMouseLeave={() => {
+                    setHover(false);
+                }}
             >
-                <div className="flex flex-row justify-between">
-                    <div className={`text-[1vw] ${montserrat.className}`}>
-                        {date}
-                    </div>
-                    <div className={`text-[1vw] ${montserrat.className}`}>
-                        {type}
-                    </div>
-                </div>
-                <div className="flex flex-row mt-[1vh] justify-between items-center">
-                    <div className={`${opensans.className} text-[3vw] `}>
-                        {name}
-                    </div>
-                    <motion.div
-                        animate={{ rotate: hover ? -45 : 0 }}
-                        className="w-[3vw] h-[3vw] aspect-square"
-                    >
-                        <ArrowForward className="text-white text-[3vw]" />
-                    </motion.div>
-                </div>
-                {videos[0] ? (
-                    videos[0][0] !== "h" ? (
-                        <div className="pt-[2vh] pb-[2vh]">
-                            <video
-                                fluid={false}
-                                controls={false}
-                                muted
-                                preload="none"
-                                autoPlay
-                                className="h-[20vw] w-full object-cover object-[50%_25%] rounded-lg"
-                            >
-                                <source src={videos[0]} type="video/mp4" />
-                                Your browser does not support the video tag.
-                            </video>
+                <div
+                    className={`${color} w-[50vw] rounded-lg p-[1.5vw] h-[30vw] shadow-[0px_2vh_3vh_0.5vh_#fff]`}
+                >
+                    <div className="flex flex-row justify-between">
+                        <div className={`text-[1vw] ${montserrat.className}`}>
+                            {date}
                         </div>
+                        <div className={`text-[1vw] ${montserrat.className}`}>
+                            {type}
+                        </div>
+                    </div>
+                    <div className="flex flex-row mt-[1vh] justify-between items-center">
+                        <div className={`${opensans.className} text-[3vw] `}>
+                            {name}
+                        </div>
+                        <motion.div
+                            animate={{ rotate: hover ? -45 : 0 }}
+                            className="w-[3vw] h-[3vw] aspect-square"
+                        >
+                            <ArrowForward className="text-white text-[3vw]" />
+                        </motion.div>
+                    </div>
+                    {videos[0] ? (
+                        videos[0][0] !== "h" ? (
+                            <div className="pt-[2vh] pb-[2vh]">
+                                <video
+                                    fluid={false}
+                                    controls={false}
+                                    muted
+                                    preload="none"
+                                    autoPlay
+                                    className="h-[20vw] w-full object-cover object-[50%_25%] rounded-lg"
+                                >
+                                    <source src={videos[0]} type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        ) : (
+                            <iframe
+                                src={videos[0]}
+                                frameborder="0"
+                                allowfullscreen
+                                className="pt-[2vh] pb-[2vh] h-[20vw] w-full object-cover object-[50%_25%] rounded-lg"
+                            />
+                        )
                     ) : (
-                        <iframe
-                            src={videos[0]}
-                            frameborder="0"
-                            allowfullscreen
-                            className="pt-[2vh] pb-[2vh] h-[20vw] w-full object-cover object-[50%_25%] rounded-lg"
+                        <div
+                            className="h-[20vw] w-full rounded-lg pt-[2vh] pb-[2vh]bg-center bg-cover"
+                            style={{
+                                backgroundImage: `url(${images[0].src})`,
+                            }}
                         />
-                    )
-                ) : (
-                    <div
-                        className="h-[20vw] w-full rounded-lg pt-[2vh] pb-[2vh]bg-center bg-cover"
-                        style={{
-                            backgroundImage: `url(${images[0].src})`,
-                        }}
-                    />
-                )}
-            </div>
-        </motion.div>
+                    )}
+                </div>
+            </motion.div>
+        </Link>
     );
 };
 

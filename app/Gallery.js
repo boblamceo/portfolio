eee8df4a5ec70b66ad886ad50329810513e67ce5
boblamceo <boblam.ceo@gmail.com>
2024-08-04 17:31:45 +0800

    center gallery

--- app/Gallery.js
+++ app/Gallery.js
@@ -10,72 +10,27 @@ import "lightgallery/css/lg-thumbnail.css";
 
 const Gallery = ({ images, videos }) => {
     return (
-        <div>
-            {/* <LightGallery plugins={[lgThumbnail, lgZoom]} mode={"lg-slide"}>
-                {images.map((curr) => (
-                <a
-                    data-lg-size="1400-1400"
-                    className="m-1.5"
-                    data-iframe="true"
-                    data-src="https://www.lightgalleryjs.com/pdf/sample.pdf"
-                >
-                    <img
-                        className="img-responsive"
-                        src="https://images.unsplash.com/photo-1455541504462-57ebb2a9cec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&q=80"
-                    />
-                </a>
-                
-                ))}
-            </LightGallery> */}
+        <div className="bg-red-400 w-[70vw] flex items-center justify-center mx-auto">
             <LightGallery
-                mode="lg-fade"
-                speed={500}
                 plugins={[lgThumbnail, lgZoom]}
+                width={`1000px`}
+                mode="lg-fade"
+                onInit={() => {
+                    console.log("hi");
+                }}
             >
-                <a
-                    data-lg-size="1406-1390"
-                    className="inline-block"
-                    data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-                    data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-                >
-                    <img
-                        className="img-responsive"
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-                    />
-                </a>
-                <a
-                    data-lg-size="1406-1390"
-                    className="inline-block"
-                    data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-                    data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-                >
-                    <img
-                        className="img-responsive"
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-                    />
-                </a>
-                <a
-                    data-lg-size="1406-1390"
-                    className="inline-block"
-                    data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-                    data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-                >
-                    <img
-                        className="img-responsive"
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-                    />
-                </a>
-                <a
-                    data-lg-size="1406-1390"
-                    className="inline-block"
-                    data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
-                    data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-                >
-                    <img
-                        className="img-responsive"
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-                    />
-                </a>
+                {images.map((curr) => (
+                    <a
+                        data-lg-size="1406-1390"
+                        className="inline-block mx-auto"
+                        data-src={curr}
+                    >
+                        <img
+                            className="img-responsive h-[50vh] mx-auto"
+                            src={curr}
+                        />
+                    </a>
+                ))}
             </LightGallery>
         </div>
     );

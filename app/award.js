1dd5355f23de1b571be7542f2db52b2d1fce8396
boblamceo <boblam.ceo@gmail.com>
2024-08-07 09:51:27 +0800

    Calculated scroll

--- app/award.js
+++ app/award.js
@@ -16,98 +16,94 @@ const range = (val, min, max) => (val - min) / (max - min);
 const Award = ({ curr, index }) => {
     const [opacity, setOpacity] = useState(0);
     const ref = useRef(null);
+    const dummyRef = useRef(null);
     const handleScroll = () => {
-        if (ref.current) {
+        if (ref.current && dummyRef.current) {
             const top =
-                ref.current.getBoundingClientRect().top + window.scrollY;
+                dummyRef.current.getBoundingClientRect().top + window.scrollY;
             const cardBottom = top + window.innerHeight;
-            setOpacity(range(scrollY, cardBottom, top) * -1);
-            console.log(
-                1 - range(scrollY, top, cardBottom),
-                scrollY,
-                top,
-                cardBottom
-            );
+            setOpacity(1 - range(scrollY, top, cardBottom));
         }
     };
     useEffect(() => {
-        if (index === 0) {
-            window.addEventListener("scroll", handleScroll);
-            return () => window.removeEventListener("scroll", handleScroll);
-        }
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
     }, []);
     return (
-        <div
-            ref={ref}
-            className={`w-screen flex flex-col ${
-                index % 2 === 0 ? "items-start" : "items-end"
-            } p-[5vw] sticky top-0 h-screen`}
-            style={{
-                opacity: 1,
-            }}
-        >
-            {index === 10 ? (
-                <LightGallery
-                    plugins={[lgZoom, lgThumbnail]}
-                    mode="lg-fade"
-                    onBeforeOpen={() => {
-                        setClickIn(true);
-                    }}
-                    onBeforeClose={() => {
-                        setClickIn(false);
-                    }}
-                >
-                    {curr.awards.map((image, imageIndex) => (
+        <>
+            <div className="absolute h-screen w-screen" ref={dummyRef}></div>
+            <div
+                ref={ref}
+                className={`w-screen flex flex-col ${
+                    index % 2 === 0 ? "items-start" : "items-end"
+                } p-[5vw] sticky top-0 h-screen`}
+                style={{
+                    opacity,
+                }}
+            >
+                {index === 10 ? (
+                    <LightGallery
+                        plugins={[lgZoom, lgThumbnail]}
+                        mode="lg-fade"
+                        onBeforeOpen={() => {
+                            setClickIn(true);
+                        }}
+                        onBeforeClose={() => {
+                            setClickIn(false);
+                        }}
+                    >
+                        {curr.awards.map((image, imageIndex) => (
+                            <a
+                                className=" inline-block rounded-[1vw]"
+                                data-src={image}
+                                key={image}
+                            >
+                                <img
+                                    className={`img-responsive ${
+                                        imageIndex === 0
+                                            ? "max-h-[50vh]"
+                                            : "max-h-0"
+                                    } rounded-[1vw] shadow-[0px_2vh_3vh_0.5vh_#fff] hover:cursor-pointer`}
+                                    src={image}
+                                ></img>
+                            </a>
+                        ))}
+                    </LightGallery>
+                ) : (
+                    <LightGallery
+                        plugins={[lgZoom, lgThumbnail]}
+                        mode="lg-fade"
+                        onBeforeOpen={() => {
+                            setClickIn(true);
+                        }}
+                        onBeforeClose={() => {
+                            setClickIn(false);
+                        }}
+                    >
                         <a
-                            className=" inline-block rounded-[1vw]"
-                            data-src={image}
-                            key={image}
+                            className="inline-block rounded-[1vw]"
+                            data-src={curr.awards}
+                            key={curr.awards}
                         >
                             <img
-                                className={`img-responsive ${
-                                    imageIndex === 0
-                                        ? "max-h-[50vh]"
-                                        : "max-h-0"
-                                } rounded-[1vw] shadow-[0px_2vh_3vh_0.5vh_#fff] hover:cursor-pointer`}
-                                src={image}
+                                className="img-responsive max-h-[60vh] rounded-[1vw] hover:cursor-pointer shadow-[0px_2vh_3vh_0.5vh_#fff]"
+                                src={index === 11 ? CCCThumb : curr.awards}
                             ></img>
                         </a>
-                    ))}
-                </LightGallery>
-            ) : (
-                <LightGallery
-                    plugins={[lgZoom, lgThumbnail]}
-                    mode="lg-fade"
-                    onBeforeOpen={() => {
-                        setClickIn(true);
-                    }}
-                    onBeforeClose={() => {
-                        setClickIn(false);
-                    }}
+                    </LightGallery>
+                )}
+                <div
+                    className={`text-white ${
+                        montserrat.className
+                    } text-[2.5vh] h-[20vh] w-[40vw] ${
+                        index % 2 === 0 ? "text-left" : "text-right"
+                    } flex flex-col justify-end`}
                 >
-                    <a
-                        className="inline-block rounded-[1vw]"
-                        data-src={curr.awards}
-                        key={curr.awards}
-                    >
-                        <img
-                            className="img-responsive max-h-[60vh] rounded-[1vw] hover:cursor-pointer shadow-[0px_2vh_3vh_0.5vh_#fff]"
-                            src={index === 11 ? CCCThumb : curr.awards}
-                        ></img>
-                    </a>
-                </LightGallery>
-            )}
-            <div
-                className={`text-white ${
-                    montserrat.className
-                } text-[2.5vh] h-[20vh] w-[40vw] ${
-                    index % 2 === 0 ? "text-left" : "text-right"
-                } flex flex-col justify-end`}
-            >
-                <div className="italic font-bold    ">{curr.date}</div>
-                {curr.details}
+                    <div className="italic font-bold    ">{curr.date}</div>
+                    {curr.details}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 

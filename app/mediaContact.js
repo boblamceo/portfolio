a1ed211cfc6c80e0a67ac9328b8be77441156d2a
boblamceo <boblam.ceo@gmail.com>
2024-08-10 18:38:07 +0800

    made hover animation

--- app/mediaContact.js
+++ app/mediaContact.js
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
 import { Montserrat } from "next/font/google";
 import React from "react";
 
@@ -10,18 +11,22 @@ const montserrat = Montserrat({
 const MediaContact = ({ icon, title, username, link }) => {
     return (
         <div className="w-[30vw] flex flex-row items-center mb-[2vw]">
-            <div className="w-[6vw] h-[6vw] border-gray-400 border-[1px] flex justify-center items-center mr-[2vw]">
+            <motion.div
+                className="w-[6vw] h-[6vw] border-gray-400 border-[1px] flex justify-center items-center mr-[2vw]"
+                initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                whileHover={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
+            >
                 <FontAwesomeIcon
                     icon={icon}
                     className="text-[3vw] text-gray-400"
                 />
-            </div>
+            </motion.div>
             <div>
-                <div
+                <motion.div
                     className={`${montserrat.className} text-[2vw] font-bold text-gray-400`}
                 >
                     {title}
-                </div>
+                </motion.div>
                 <div
                     className={`${montserrat.className} text-[1.5vw]  text-gray-400`}
                 >
